844687d2b00bdef638de071fa6c417ed2408363b
Lupus Hortian <git@hortien.com>
2024-12-12 23:54:56 +0100

    Add MatchesFromGunpowder and MatchesFromStumpRemover

---
+++ Build.cmd
@@ -0,0 +1,4 @@
+del .\modcomponent\Lupus_UsefulBlueprints.modcomponent
+c:\tools\7zip\7z a .\modcomponent\Lupus_UsefulBlueprints.modcomponent BuildInfo.json
+c:\tools\7zip\7z a .\modcomponent\Lupus_UsefulBlueprints.modcomponent blueprints
+
