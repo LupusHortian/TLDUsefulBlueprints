32961072feb3693a298eae2de922475ed5b79a8b
Lupus Hortian <git@hortien.com>
2024-12-27 18:08:54 +0100

    Salt Bag from Salt and Cloths

--- Build.cmd
+++ Build.cmd
@@ -1,4 +1,8 @@
-del .\modcomponent\Lupus_UsefulBlueprints.modcomponent
-c:\tools\7zip\7z a .\modcomponent\Lupus_UsefulBlueprints.modcomponent BuildInfo.json
-c:\tools\7zip\7z a .\modcomponent\Lupus_UsefulBlueprints.modcomponent blueprints
-
+@echo off
+echo Pack buildinfo file
+c:\tools\7zip\7z -y a .\modcomponent\Lupus_UsefulBlueprints.modcomponent BuildInfo.json 1>nul
+echo Pack blue prints
+c:\tools\7zip\7z -y a .\modcomponent\Lupus_UsefulBlueprints.modcomponent blueprints 1>nul
+echo Copy file into Mod folder
+copy /y .\modcomponent\Lupus_UsefulBlueprints.modcomponent f:\Spiele\Steam\steamapps\common\TheLongDark\Mods\ 1>nul
+echo All done.
